341883134d12bd9966f587b3517f914257a3bb9b
niko immonen <niko.immonen@itvelhot.local>
2019-01-09 11:40:42 +0200

    palautelomakkeen toteutus projekti valmis

--- php-palautekomakkeen_toteutus/get_input.php
+++
@@ -1,3 +0,0 @@
-<?php
-$xml = simplexml_save_file('data/lomake.xml');
-?>
--- php-palautekomakkeen_toteutus/tallennaTiedot.php
+++ php-palautekomakkeen_toteutus/tallennaTiedot.php
@@ -1,3 +1,23 @@
 <?php
-$xml = simplexml_save_file('data/lomake.xml');
+
+$nimi = $_POST['nimi'];
+$sahkoposti = $_POST['sahkoposti'];
+$palaute = $_POST['palaute'];
+$aika = $_POST['aika'];
+
+define('XML_FILE', 'data_lomake.xml');
+$xml = simplexml_load_file(XML_FILE);
+
+$uusi_rivi = $xml->addchild('rivi');
+$uusi_rivi->addchild('nimi', $nimi);
+$uusi_rivi->addchild('sahkoposti', $sahkoposti);
+$uusi_rivi->addchild('palaute', $palaute);
+$uusi_rivi->addchild('aika', $aika);
+
+$dom = new DOMDocument('1.0');
+$dom->preserveWhiteSpace = false;
+$dom->formatOutput = true;
+$dom->loadXML($xml->asXML());
+$dom->save(XML_FILE);
+
 ?>
