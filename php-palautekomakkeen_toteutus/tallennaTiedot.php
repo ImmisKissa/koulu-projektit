517410825824f808520fa8dc6ae1357ad14b1515
niko immonen <niko.immonen@itvelhot.local>
2019-01-18 12:39:07 +0200

    php-palutelomakkeentoteutus valmis

--- php-palautekomakkeen_toteutus/tallennaTiedot.php
+++ php-palautekomakkeen_toteutus/tallennaTiedot.php
@@ -1,9 +1,9 @@
 <?php
 
-$nimi = $_POST['nimi'];
-$sahkoposti = $_POST['sahkoposti'];
-$palaute = $_POST['palaute'];
-$aika = $_POST['aika'];
+$nimi = $_GET['nimi'];
+$sahkoposti = $_GET['sahkoposti'];
+$palaute = $_GET['palaute'];
+$date =  date("d-m-Y h:i:s");
 
 define('XML_FILE', 'data_lomake.xml');
 $xml = simplexml_load_file(XML_FILE);
@@ -12,12 +12,15 @@ $uusi_rivi = $xml->addchild('rivi');
 $uusi_rivi->addchild('nimi', $nimi);
 $uusi_rivi->addchild('sahkoposti', $sahkoposti);
 $uusi_rivi->addchild('palaute', $palaute);
-$uusi_rivi->addchild('aika', $aika);
+$uusi_rivi->addchild('date', $date);
 
 $dom = new DOMDocument('1.0');
 $dom->preserveWhiteSpace = false;
 $dom->formatOutput = true;
 $dom->loadXML($xml->asXML());
 $dom->save(XML_FILE);
+
+header("Location: show_xml.html");
+exit();
 
 ?>
